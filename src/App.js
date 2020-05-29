b5e114fbae66b5b26181de9dfabab51cc0b5730f
Alex <alex.andrul10@gmail.com>
2020-05-29 14:18:01 +0100

    dispaly countries

--- src/App.js
+++ src/App.js
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import Countries from "./Components/Countries";
 
 function App() {
+  const [data, setData] = useState([]);
+  const [hasError, setErrors] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function fetchData(url) {
+    setIsLoading(true);
+    await fetch(url)
+      .then((response) => {
+        if (!response.ok) {
+          throw Error("Sorry! We didn't find any country");
+        }
+        return response.json();
+      })
+      .then((result) => setData(result))
+      .catch((err) => setErrors(err));
+    setIsLoading(false);
+  }
+  useEffect(() => {
+    fetchData("https://restcountries.eu/rest/v2/all");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h3>Where in the world?</h3>
-        <a>
+        <a className="darkBtn" href="/">
           <i className="fas fa-moon"></i>Dark Mode
         </a>
       </header>
-      <Countries />
+      <div className="main-container">
+        <Countries data={data} err={hasError} loading={isLoading} />
+      </div>
     </div>
   );
 }
--- src/Components/Countries.js
+++ src/Components/Countries.js
@@ -1,7 +1,30 @@
 import React from "react";
+import CountryCard from "./CountryCard";
 
-const Countries = () => {
-  return <div>Alex</div>;
+function errorData() {
+  return (
+    <div>
+      <p>404</p>
+      <h1>Sorry!</h1>
+      <p>Something went wrong!</p>
+    </div>
+  );
+}
+const Countries = (props) => {
+  return (
+    <div>
+      {props.err && errorData()}
+      {props.loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div className="container">
+          {props.data.map((countries, index) => (
+            <CountryCard key={index} data={countries} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Countries;
