189f9d4be64647eccbccf3bfa7191dba5bb4f34d
Alex <35708592+AlexandruBudaca@users.noreply.github.com>
2020-06-18 19:50:03 +0100

    country detail

--- src/Components/CountryDetail.js
+++ src/Components/CountryDetail.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const CountryDetail = ({ data, setCountryName, isLoaded }) => {
   return (
-    <div>
+    <div className="container">
       {!isLoaded ? (
         <div className="loading">
           <h1>Loading...</h1>
@@ -16,7 +16,56 @@ const CountryDetail = ({ data, setCountryName, isLoaded }) => {
           >
             Back
           </button>
-          <div>{data[0].name}</div>
+          <div className="country-detail">
+            <div>
+              <img src={data[0].flag} alt="country" />
+            </div>
+            <div>
+              <h3>{data[0].name}</h3>
+              <p>
+                <span>Native name:</span> {data[0].nativeName}
+              </p>
+              <p>
+                <span>Population: </span>{" "}
+                {data[0].population
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </p>
+              <p>
+                <span>Region: </span>
+                {data[0].region}
+              </p>
+              <p>
+                <span>Sub Region: </span>
+                {data[0].subregion}
+              </p>
+              <p>
+                <span>Capital: </span> {data[0].capital}
+              </p>
+            </div>
+
+            <div className="top-domains">
+              <br />
+              <br />
+              <div>
+                <p>
+                  <span>Top Level Domain:</span> {data[0].topLevelDomain}
+                </p>
+                <p>
+                  <span>Currencies: </span>
+                  {data[0].currencies[0].code}
+                </p>
+                <div className="Languages">
+                  <span> Languages:</span>
+                  {data[0].languages.map((lang, index) => lang.name).join(", ")}
+                </div>
+              </div>
+
+              <div>
+                <p>sadadas</p>
+              </div>
+            </div>
+          </div>
         </div>
       )}
     </div>
